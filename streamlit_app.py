3bcf7e71e1de5c8a689da76689dfc6f118d0829a
Srinathvaida <82866919+Srinathvaida@users.noreply.github.com>
2024-08-27 15:24:19 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,16 @@
 import streamlit as st
-
-st.title('🎈 App Name')
-
-st.write('Hello world!')
+import numpy as np
+import pandas as pd
+st.title("Hello World!")
+st.header("This is a header with a divider", divider="rainbow")
+st.markdown("*Streamlit* is **really** ***cool***.")
+col1,col2 = st.columns(2)
+with col1:
+    st.header("st.column")
+    x = st.slider("mark the x value:",0,10)
+with col2:
+    
+    st.write("the value of :red[***x***] is",x)
+st.header("st.area_chart")
+chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
+st.area_chart(chart_data)
